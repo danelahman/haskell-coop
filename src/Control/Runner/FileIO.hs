94b002732c54bd91783f441a4a2a306158e6a122
Danel Ahman <danel.ahman@eesti.ee>
2019-10-22 10:53:21 +0200

    Documentation

--- src/Control/Runner/FileIO.hs
+++ src/Control/Runner/FileIO.hs
@@ -3,10 +3,19 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
 
---
--- Experiments with File IO using a variety of runners.
---
+{-|
+Module      : Control.FileIO
+Description : Runners implementing file IO
+Copyright   : (c) Danel Ahman, 2019
+License     : MIT
+Maintainer  : danel.ahman@eesti.ee
+Stability   : experimental
 
+This module provides a variety of runners implementing file IO. 
+These runners mostly differ in what they store in their runtime 
+state, e.g., storing a file handle vs storing the accumulated 
+writes to a file. 
+-}
 module Control.Runner.FileIO (
   FileIO(..), fOpenOS, fCloseOS, fReadOS, fWriteOS,
   File(..), fRead, fWrite, FIOState, FHState, FCState, 
@@ -24,43 +33,50 @@ import qualified Data.ByteString.Char8 as B
 --
 -- Signature of the file-fragment of the external world.
 --
-data FileIO :: * -> * where
+
+-- | An effect for performing file IO.
+data FileIO a where
+  -- | Algebraic operation for opening a file in a given mode.
   OpenFile  :: FilePath -> IOMode -> FileIO Handle
+  -- | Algebraic operation of closing a given file.
   CloseFile :: Handle -> FileIO ()
+  -- | Algebraic operation for reading from a given file.
   ReadFile  :: Handle -> FileIO String
+  -- | Algebraic operation for writing to a given file.
   WriteFile :: Handle -> String -> FileIO ()
 
---
--- Generic effects.
---
+-- | Generic effect for opening a file in a given mode.
 fOpenOS :: (Member FileIO sig) => FilePath -> IOMode -> User sig Handle
 fOpenOS fn mode = focus (performU (OpenFile fn mode))
 
+-- | Generic effect for closing a given file.
 fCloseOS :: (Member FileIO sig) => Handle -> User sig ()
 fCloseOS fh = focus (performU (CloseFile fh))
 
+-- | Generic effect for reading from a given file.
 fReadOS :: (Member FileIO sig) => Handle -> User sig String
 fReadOS fh = focus (performU (ReadFile fh))
 
+-- | Generic effect for writing to a given file.
 fWriteOS :: (Member FileIO sig) => Handle -> String -> User sig ()
 fWriteOS fh s = focus (performU (WriteFile fh s))
 
+-- | An effect for performing reads and writes (on a file whose file
+-- handle is hidden by the user code through the use of runners).
 --
--- Signature for reading from and writing to files.
---
--- In the runners below, Read denotes reading the
--- initial value of a file when using a runner.
---
-data File r where
+-- In this module, we additionally suppose that Read denotes 
+-- reading the initial value of a file when using a runner.
+data File a where
+  -- | Algebraic operation for reading (from a file that is hidden from user code).
   Read :: File String
+  -- | Algebraic operation for writing (to a file that is hidden from user code).
   Write :: String -> File ()
 
---
--- Generic effects.
---
+-- | Generic effect for reading (from a file that is hidden from user code).
 fRead :: (Member File sig) => User sig String
 fRead = focus (performU Read)
 
+-- | Generic effect for writing (to a file that is hidden from user code).
 fWrite :: (Member File sig) => String -> User sig ()
 fWrite s = focus (performU (Write s))
 
@@ -70,8 +86,14 @@ fWrite s = focus (performU (Write s))
 -- The state of FIO is trivial because we cannot
 -- internally access nor represent the real world.
 --
+
+-- | Type of the runtime state of the runner `fioRunner`.
+-- 
+-- The state is trivial because this runner directly delegates
+-- the file IO operations to Haskell's `IO` monad operations.
 type FIOState = ()
 
+-- | The co-operations of the runner `fioRunner`.
 fioCoOps :: Member IO sig => FileIO a -> Kernel sig FIOState a
 fioCoOps (OpenFile fn mode) =
   user (focus (performU (openFile fn mode))) return
@@ -83,17 +105,21 @@ fioCoOps (ReadFile fh) =
 fioCoOps (WriteFile fh s) =
   user (focus (performU (B.hPutStr fh (B.pack s)))) return
 
+-- | Runner that implements the `FileIO` effect, by delegating
+-- the file IO operations to Haskell's `IO` monad operations.
+--
+-- Intuitively, this runner focusses on a fraction of the larger,
+-- external signature (namely, that of the `IO` monad).
 fioRunner :: Member IO sig =>  Runner '[FileIO] sig FIOState
 fioRunner = mkRunner fioCoOps
 
+-- | Type of the runtime state of the runner `fhRunner`.
 --
--- FH: File-runner that operates on a single file handle.
---
--- The state of FH is the initial contents of the file and
--- then a file handle supporting (over)writing to the file.
---
+-- The state comprises the initial contents of the file
+-- and then a file handle supporting (over)writing to the file.
 type FHState = (String , Handle)
 
+-- | Type co-operations of the runner `fhRunner`.
 fhCoOps :: Member FileIO sig => File a -> Kernel sig FHState a
 fhCoOps Read =
   do (s,fh) <- getEnv;
@@ -102,6 +128,11 @@ fhCoOps (Write s') =
   do (s,fh) <- getEnv;
      user (focus (performU (WriteFile fh s'))) return
 
+-- | Runner that implements the `File` effect, by
+-- returning the internally stored (initial) contents
+-- on `Read` operations, and delegates `Write` operations
+-- to some enveloping runner for the `FileIO` effect,
+-- using the file handle stored in its runtime state.
 fhRunner :: Member FileIO sig => Runner '[File] sig FHState
 fhRunner = mkRunner fhCoOps
 
@@ -111,8 +142,15 @@ fhRunner = mkRunner fhCoOps
 -- The state of FC is the initial contents of the file and
 -- then the contents to be written to the file in finally.
 --
+
+-- | Type of the runtime state of the runner `fcRunner`.
+--
+-- The state comprises the initial contents of the file,
+-- and then an accumulator for strings to be written to
+-- the file in the finalisation (when running with `fcRunner`).
 type FCState = (String , String)
 
+-- | The co-operations of the runner `fcRunner`.
 fcCoOps :: File a -> Kernel sig FCState a
 fcCoOps Read =
   do (s,s') <- getEnv;
@@ -121,26 +159,36 @@ fcCoOps (Write s'') =
   do (s,s') <- getEnv;
      setEnv (s,s' ++ s'')
 
+-- | Runner that implements the `File` effect,
+-- by returning the internally stored (initial)
+-- contents on `Read` operations, and accumulates
+-- any `Write` operations in its runtime state.
 fcRunner :: Runner '[File] sig FCState
 fcRunner = mkRunner fcCoOps
 
 --
 -- IO <-> FIO.
 --
+
+-- | Initialiser for the runner `fioRunner`
+-- in the `IO` monad external context. 
 ioFioInitialiser :: Member IO sig => User sig FIOState
 ioFioInitialiser = return ()
 
+-- | Finaliser for the runner `fioRunner`
+-- in the `IO` monad external context.
+--
+-- As the runtime state of the `fioRunner` is trivial,
+-- the finaliser simply passes on the return value.
 ioFioFinaliser :: Member IO sig => a -> FIOState -> User sig a
 ioFioFinaliser x _ = return x
 
+-- | Initialiser for the runner `fhRunner`,
+-- in the `FileIO` effect external context.
 --
--- FIO <-> FH.
---
--- Initialiser: The contents of the file is read, and 
--- a file handle for writing to the file is opened.
---
--- Finaliser: The file handle is closed.
---
+-- It first reads the initial contents of the given 
+-- file and then it opens the file for writing,
+-- returning the initial contents and the file handle.
 fioFhInitialiser :: Member FileIO sig => FilePath -> User sig FHState
 fioFhInitialiser fn =
   do fh <- fOpenOS fn ReadWriteMode;
@@ -149,24 +197,32 @@ fioFhInitialiser fn =
      fh <- fOpenOS fn WriteMode;
      return (s,fh)
 
+-- | Finaliser for the runner `fhRunner`,
+-- in the `FileIO` effect external context.
+--
+-- It closes the file given file handle, and passes
+-- on the return value.
 fioFhFinaliser :: Member FileIO sig => a -> FHState -> User sig a
 fioFhFinaliser x (_,fh) =
   do fCloseOS fh;
      return x
   
+-- | Initialiser for the runner `fcRunner`,
+-- in the `File` effect external context.
 --
--- FH <-> FC.
---
--- Initialiser: The contents of the file is read, and 
--- the new contents to be written is initialised to "".
---
--- Finaliser: The new contents is written to the file.
---
+-- It first reads the initial contents of the given 
+-- file, and then returns the contents and the empty
+-- accumulator for `Write` operations.
 fhFcInitialiser :: Member File sig => User sig FCState
 fhFcInitialiser =
   do s <- fRead;
      return (s,"")
 
+-- | Finaliser for the runner `fcRunner`,
+-- in the `File` effect external context.
+--
+-- It writes the accumulated writes with `Write`
+-- and passes on the return value.
 fhFcFinaliser :: Member File sig => a -> FCState -> User sig a
 fhFcFinaliser x (_,s) =
   do fWrite s;
@@ -176,6 +232,18 @@ fhFcFinaliser x (_,s) =
 -- Derived with-file construct using the
 -- composite IO <-> FIO <-> FH <-> FC.
 --
+
+-- | Derived with-file construct that runs user code with
+-- the `File` effect in the external context of the `IO`
+-- monad.
+--
+-- This construct nests all the different runners implemented
+-- in this module, as follows
+--
+-- > IO monad <-> fioRunner <-> fhRunner <-> fcRunner <-> user code
+--
+-- with the arrows informally denoting the various initialisers
+-- and finalisers for the different runners.
 withFile :: FilePath -> User '[File] a -> User '[IO] a
 withFile fn m =
   run
